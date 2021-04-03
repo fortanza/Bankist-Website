2fa92fb0eed627a4e902c6ab84aa0970951112f4
fortanza <undertaker040880@gmail.com>
2021-04-03 17:40:00 +0200

    190. DOM Traversing

--- script.js
+++ script.js
@@ -227,3 +227,36 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
 //   this.style.backgroundColor = randomColor();
 //   console.log('NAV', e.target, e.currentTarget);
 // });
+
+// 190. DOM Traversing
+
+const h1 = document.querySelector('h1');
+
+//going downwards: child
+
+console.log(h1.querySelectorAll('.highlight'));
+console.log(h1.childNodes);
+console.log(h1.children);
+h1.firstElementChild.style.color='red';
+h1.lastElementChild.style.color='purple';
+
+//going upwards: parents
+console.log(h1.parentNode);
+
+h1.closest('.header').style.background= "var(--gradient-secondary)"
+
+h1.closest('h1').style.background= "var(--gradient-primary"
+
+// Going sideways : sibling
+
+console.log(h1.previousElementSibling);
+console.log(h1.nextElementSibling);
+
+console.log(h1.previousSibling);
+console.log(h1.nextSibling);
+
+console.log(h1.parentElement.children);
+
+[...h1.previousElement.childen].forEach(function(el){
+  if (el !== h1 ) el.style.transform = 'scale(0.5)';
+})
